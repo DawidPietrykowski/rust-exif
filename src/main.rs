572eb09105b656ca720449a34663f814a8c47245
Dawid Pietrykowski <d.pietrykowski16@gmail.com>
2023-09-25 23:38:12 +0200

    Working basic functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,54 @@
-use std::{fs, io};
-use std::ffi::OsStr;
-use std::fs::DirEntry;
-use std::path::{Path, PathBuf};
-use exif::{Context, In, Tag};
 use rexiv2::Metadata;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
 
 const ALLOWED_EXTENSIONS: [&str; 6] = ["heic", "jpg", "jpeg", "png", "arw", "dng"];
 
-fn is_image(filename: &PathBuf) -> bool{
-    if filename.file_name().unwrap().to_str().unwrap().starts_with("."){
+fn is_image(filename: &PathBuf) -> bool {
+    if filename
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .starts_with(".")
+    {
         return false;
     }
-    let ext = filename.extension().unwrap_or(OsStr::new("")).to_str().unwrap();
+
+    let ext = filename
+        .extension()
+        .unwrap_or(OsStr::new(""))
+        .to_str()
+        .unwrap();
     let lower_passed = ext.to_lowercase();
     for allowed_extension in ALLOWED_EXTENSIONS {
         let lower_allowed = allowed_extension.to_lowercase();
-        if lower_allowed == lower_passed{
+        if lower_allowed == lower_passed {
             return true;
         }
     }
     false
 }
 
-fn visit_dirs(dir: &Path, paths: &mut Vec<PathBuf>, depth: i32, x: fn(&str) -> bool) -> io::Result<()> {
+fn visit_dirs(
+    dir: &Path,
+    paths: &mut Vec<PathBuf>,
+    depth: i32,
+    x: fn(&str) -> bool,
+) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                let dir_name = path.as_path().file_name().expect("Could not get relative path").to_str().unwrap();
-                if (depth != 0 || x(dir_name)) && !dir_name.starts_with("."){
+                let dir_name = path
+                    .as_path()
+                    .file_name()
+                    .expect("Could not get relative path")
+                    .to_str()
+                    .unwrap();
+                if (depth != 0 || x(dir_name)) && !dir_name.starts_with(".") {
                     // filter
                     if depth == 0 {
                         println!("Passed {dir_name}");
@@ -38,8 +57,7 @@ fn visit_dirs(dir: &Path, paths: &mut Vec<PathBuf>, depth: i32, x: fn(&str) -> b
                 }
             } else {
                 let path_buf = entry.path();
-                if is_image(&path_buf)
-                {
+                if is_image(&path_buf) {
                     paths.push(path_buf);
                 }
             }
@@ -48,75 +66,149 @@ fn visit_dirs(dir: &Path, paths: &mut Vec<PathBuf>, depth: i32, x: fn(&str) -> b
     Ok(())
 }
 
-fn starts_with_2210(string: &str) -> bool {
-    // (string.starts_with("23") || string.starts_with("22")
-    //     || string.starts_with("21-11") || string.starts_with("21-09"))
-    //     && !string.contains("21-09-00-Warsaw")
-    string.contains("KomuniaKuby")
+fn filter_string(string: &str, excluded_paths: Vec<String>) -> bool {
+    for path in excluded_paths {
+        if string.contains(path.as_str()) {
+            return false;
+        }
+    }
+    true
+}
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: FileCommand,
+
+    #[arg(short = 't', long, default_value_t = 5)]
+    threshold: i32,
+
+    #[arg(short = 'i', long, default_value_t = false)]
+    inverse: bool,
+
+    #[arg(short = 'v', long, default_value_t = false)]
+    verbose: bool,
+
+    #[arg(short = 'd', long)]
+    dest: std::path::PathBuf,
+
+    #[arg(short = 's', long)]
+    src: std::path::PathBuf,
+}
+
+#[derive(Subcommand)]
+enum FileCommand {
+    Move,
+    Copy,
+    Delete,
 }
 
 fn main() {
+    let cli: Cli = Cli::parse();
+
     rexiv2::initialize().expect("Unable to initialize rexiv2");
+
+    let command_name = match cli.command {
+        FileCommand::Move => "Moving",
+        FileCommand::Copy => "Copying",
+        FileCommand::Delete => "Deleting",
+    };
+
+    let search_path = cli.src;
+    let output_path: PathBuf = cli.dest;
+
+    assert!(search_path.is_dir(), "Source path must be a directory");
+
+    if output_path.exists() {
+        assert!(output_path.is_dir(), "Output path must be a directory");
+    } else {
+        fs::create_dir(output_path.clone()).expect("Failed to create output directory");
+    }
+
     let mut all_paths: Vec<PathBuf> = Vec::new();
-    let search_path = "/media/nfs/sphotos/Images/22-07-01-London/";
-    let output_path = "/media/nfs/tmp/22-07-01-London-5star/";
-    visit_dirs(search_path.as_ref(), &mut all_paths, 1, |_| true).expect("TODO: panic message");
-    for path in all_paths{
-        let filename = path.as_path().to_str().unwrap();
-        let mut root_directory = path.as_os_str().to_str().unwrap().chars().skip(search_path.len()).collect::<String>();
-        if let Some(pos) = root_directory.find("/"){
-            root_directory = root_directory.chars().take(pos).collect::<String>();
-        }
-        let res = get_rating(filename.clone());
+    visit_dirs(search_path.as_ref(), &mut all_paths, 1, |_| true)
+        .expect("Failed to iterate over directories");
+
+    for path in all_paths {
+        let relative_path = path
+            .strip_prefix(search_path.clone())
+            .expect(format!("Failed to strip root prefix of file {:?}", path).as_str());
+        let new_file_path = output_path.join(&relative_path);
+
+        let res: Result<i32, String> = get_rating(path.clone());
         let Ok(rating) = res else{
-            println!("Skipping {filename} due to {}", res.err().unwrap_or("Unknown error".to_string()).to_string());
+            println!("Skipping {path:?} due to {}", res.err().unwrap_or("Unknown error".to_string()).to_string());
             continue;
         };
-        if rating >= 5{
-            let out_path = output_path.to_string() + &root_directory + "/" + path.clone().file_name().unwrap().to_str().to_owned().unwrap();
-            println!("{rating} {root_directory} {filename} {out_path}");
-            let dir_path = (output_path.to_string() + &root_directory);
-            if !path_exists(dir_path.as_str()){
-                fs::create_dir(dir_path.as_str()).unwrap();
+
+        let mut should_move = rating >= cli.threshold;
+        if cli.inverse {
+            should_move = !should_move;
+        }
+
+        if should_move {
+            if cli.verbose {
+                println!("Rated: {rating} {command_name} {path:?} {new_file_path:?}");
             }
-            if !path_exists(out_path.as_str()){
-                fs::copy(path.clone(), Path::new(out_path.as_str())).unwrap();
+
+            let dir_path = new_file_path.parent().unwrap();
+            if !path_exists(dir_path.to_path_buf()) {
+                fs::create_dir(dir_path.to_path_buf()).unwrap();
             }
-            if !path.as_os_str().to_str().unwrap().contains("HEIC"){
-                let jpg_suffix = match path.as_os_str().to_str().unwrap().contains(".JPG") {
-                    true => ".JPG",
-                    false => "-c.jpg"
-                };
-                let raw_path = (path.as_os_str().to_str().unwrap().strip_suffix(jpg_suffix).unwrap().to_string() + ".ARW");
-                let raw_path_str = raw_path.as_str();
-                if path_exists(raw_path_str) {
-                    let out_path_raw = out_path.strip_suffix(jpg_suffix).unwrap().to_string() + ".ARW";
-                    fs::copy(raw_path_str, Path::new(out_path_raw.as_str())).unwrap();
+            if !path_exists(new_file_path.clone()) {
+                match cli.command {
+                    FileCommand::Move => {
+                        fs::rename(path, new_file_path).unwrap();
+                    }
+                    FileCommand::Copy => {
+                        fs::copy(path, new_file_path).unwrap();
+                    }
+                    FileCommand::Delete => {
+                        fs::remove_file(path).unwrap();
+                    }
                 }
             }
+            // if !path.as_os_str().to_str().unwrap().contains("HEIC") {
+            //     let jpg_suffix = match path.as_os_str().to_str().unwrap().contains(".JPG") {
+            //         true => ".JPG",
+            //         false => "-c.jpg",
+            //     };
+            //     let raw_path = (path
+            //         .as_os_str()
+            //         .to_str()
+            //         .unwrap()
+            //         .strip_suffix(jpg_suffix)
+            //         .unwrap()
+            //         .to_string()
+            //         + ".ARW");
+            //     let raw_path_str = raw_path.as_str();
+            //     if path_exists(raw_path_str) {
+            //         let out_path_raw =
+            //             out_path.strip_suffix(jpg_suffix).unwrap().to_string() + ".ARW";
+            //         fs::copy(raw_path_str, Path::new(out_path_raw.as_str())).unwrap();
+            //     }
+            // }
         }
     }
 }
 
-pub fn path_exists(path: &str) -> bool {
+pub fn path_exists(path: PathBuf) -> bool {
     fs::metadata(path).is_ok()
 }
 
-fn get_rating(filename: &str) -> Result<i32, String> {
-    if !path_exists(filename){
+fn get_rating(filename: PathBuf) -> Result<i32, String> {
+    if !path_exists(filename.clone()) {
         return Err("File doesn't exist".to_string());
     }
 
     let meta = Metadata::new_from_path(filename);
     match meta {
-        Ok(meta) =>{
+        Ok(meta) => {
             let rating = meta.get_tag_numeric("Xmp.xmp.Rating");
-                // println!("{rating:?} {filename}");
             Ok(rating)
-        },
-        Err(e) =>{
-            Err(e.to_string())
         }
+        Err(e) => Err(e.to_string()),
     }
-
 }
